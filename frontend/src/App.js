a1604295ce53d9866a291b1dbd608bce3c4463cf
JackJames <164722047+JackJames5630@users.noreply.github.com>
2024-04-09 06:44:21 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,9 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { LOGOUT } from './utils/constant';
-//kjhikhiuhuihuihuihi
-//1123123
+
 import setAuthToken from './utils/setAuthToken';
-// i have added some changes
-// Redux
-//i have changed one.
-//ok? are uok? ok!.ok!
+
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actinos/auth';
